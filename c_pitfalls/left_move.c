f61f3b4b23039ed10ef6d5e699f7566db1890af0
zhouhao2 <zhouhao2@hisense.com>
2016-09-05 22:48:36 -0400

    add unsigned int

---
+++ c_pitfalls/left_move.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+ int i =1,j= i<<2;
+
+ i<<2;
+
+ printf("i am %d:,j is %d\n",i,j);
+
+
+ return 0;
+}
---
+++ c_pitfalls/octal_number.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+ int a = 001,b=0100,c=110;
+
+ printf("a is %d.\n,b is %d,c is %d\n",a,b,c);
+
+ return 0;
+}
