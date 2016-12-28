2fa73868c98fbf7d08a78eb659c50ff0bd0587d1
lucien <825038797@qq.com>
2016-12-28 11:50:31 +0800

    [commit type(feature or structure)]:f
    [new knowleage]:use variable args for define
    
    [some application(link or example)]: it's a regular usage
    
    [new skill(where to apply?)]: no

---
+++ function_c_c++_macro_definition/definepoint.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#define log(...) {printf(__VA_ARGS__); putchar('\n');}
+#define logargs(args...) {printf(args); putchar('\n');}
+
+int main(){
+  log("i feel good");
+  logargs("i feel good");
+  return 0;
+}
