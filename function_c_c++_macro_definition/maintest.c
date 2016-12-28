db40f7e6468127b2c15547a79747eb4c8c49f15a
lucien <825038797@qq.com>
2016-12-28 13:45:19 +0800

    [commit type(feature or structure)]:f
    [new knowleage]:deference bettween
    ifdef xx ; and
    if defined(xx);
    
    [some application(link or example)]:commom usage
    
    [new skill(where to apply?)]:no

--- function_c_c++_macro_definition/maintest.c
+++ function_c_c++_macro_definition/maintest.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
+#define _ANDROID_ android
+
+
+#define GLOBAL(...)
+
+#ifdef _ANDROID_
+  int globals=2;
+#endif
+
+#if defined(_ANDROID_)
+  int global=1;
+#else
+  int global=0
+#endif
+
+
+GLOBAL(
+int pp=5;
+char a[]="sdsd";
+);
+
 #define print printf("%s",__func__);
+
 void foo(){
-  print
-  printf("hello!!\n");
+  printf("%s\n",__func__);
 }
+
 int main(){
+  printf("%s\n",__func__);
+  printf("hello!!,global=%d,globals=%d\n",global,globals);
   foo();
   return 0;
 }
