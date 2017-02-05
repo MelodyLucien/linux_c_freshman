53a1acf2c790af206792b532881f6a12bbc0238e
wind2013 <825038797@qq.com>
2017-02-05 13:27:03 +0800

    [commit type(feature or structure)]:f
    [new knowleage]:add unlink - delete a name and possibly the file it refers to
    
    [some application(link or example)]:common
    
    [new skill(where to apply?)]:common

--- file_management/func_symlink/practices/symlink.c
+++ file_management/func_symlink/practices/symlink.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+//unlink - delete a name and possibly the file it refers to
 int main(){
 
    int res=symlink("symlink.c","sym");
@@ -9,6 +10,14 @@ int main(){
    }else{
        perror("success!");
    }
+
+   res=unlink("symlink.o");
+   if(res==1){
+       perror("failed!");
+   }else{
+       perror("success!");
+   }
+
    return 0;
 }
 
