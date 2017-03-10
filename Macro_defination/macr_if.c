be709b6715587de97d183cce4b62690ce47960ca
lucien <825038797@qq.com>
2017-03-10 14:51:34 +0800

    use addr2line to get info about readable so

--- Macro_defination/macr_if.c
+++ Macro_defination/macr_if.c
@@ -5,8 +5,19 @@
 #else 
 #define MM "elsee"
 #endif
+
+// if  __builtin_expect(a,b) both a and b is 0,the return value is 0,otherwise it will be 1
+
+#define isbuilt  __builtin_expect(0,0)
+
 int main(){
   if(1)
-  printf("%s\n",MM);
+     printf("%s\n",MM);
 
+  if(isbuilt)
+   {
+     printf("isbuilt is 1\n");
+   }else{
+     printf("isbuilt is 0\n");
+   }
 }
