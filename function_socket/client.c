214d241fae88222d71f2a5fc649ce229f886b869
Lucien <zhouhao2@hisense.com>
2017-09-10 17:36:52 +0800

    format code

--- function_socket/client.c
+++ function_socket/client.c
@@ -18,38 +18,36 @@ int main(int argc, char** argv)
 
 
     if( argc != 2){
-    printf("usage: ./client <ipaddress>\n");
-    exit(0);
+		printf("usage: ./client <ipaddress>\n");
+		exit(0);
     }
 
 
     if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
-    exit(0);
+		printf("create socket error: %s(errno: %d)\n", strerror(errno),errno);
+		exit(0);
     }
-
 
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(8000);
     if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr) <= 0){
-    printf("inet_pton error for %s\n",argv[1]);
-    exit(0);
+		printf("inet_pton error for %s\n",argv[1]);
+		exit(0);
     }
 
 
-    if( connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
-    printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
-    exit(0);
+    if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0){
+		printf("connect error: %s(errno: %d)\n",strerror(errno),errno);
+		exit(0);
     }
-
 
     printf("send msg to server: \n");
     fgets(sendline, 4096, stdin);
     if( send(sockfd, sendline, strlen(sendline), 0) < 0)
     {
-    printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
-    exit(0);
+		printf("send msg error: %s(errno: %d)\n", strerror(errno), errno);
+		exit(0);
     }
     if((rec_len = recv(sockfd, buf, MAXLINE,0)) == -1) {
        perror("recv error");
